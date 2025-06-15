a2f1ccb43c6795ce55e5221d01e3873e7e9bd83f
brandonroque808 <brandonroque808@gmail.com>
2025-06-15 19:07:31 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-import './Footer.css';
+import '../styles/Footer.css';
 
-export default function Footer() {
+function Footer() {
   return (
     <footer className="footer">
-      © 2025 Sergio el Bailador
+      <p>© 2025 Sergio el Bailador</p>
     </footer>
   );
 }
+
+export default Footer;
